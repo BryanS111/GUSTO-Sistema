bf54d0d254a77f00ac2212495494366c66256149
BryanS111 <beatsfenix5@gmail.com>
2026-06-09 12:25:30 -0600

    Implementacion de documentacion para usuarios

--- SistemaInventarioWF/frmInicio_Dashboard.Designer.cs
+++ SistemaInventarioWF/frmInicio_Dashboard.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnManualUso = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,17 +37,31 @@
             // 
             this.pictureBox1.BackgroundImage = global::SistemaInventarioWF.Properties.Resources.logo_1000x1000px;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(449, 237);
+            this.pictureBox1.Location = new System.Drawing.Point(460, 194);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(523, 513);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // btnManualUso
+            // 
+            this.btnManualUso.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnManualUso.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnManualUso.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManualUso.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnManualUso.Location = new System.Drawing.Point(558, 735);
+            this.btnManualUso.Name = "btnManualUso";
+            this.btnManualUso.Size = new System.Drawing.Size(327, 52);
+            this.btnManualUso.TabIndex = 1;
+            this.btnManualUso.Text = "Leer manual de uso...";
+            this.btnManualUso.UseVisualStyleBackColor = false;
             // 
             // frmInicio_Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1515, 983);
+            this.Controls.Add(this.btnManualUso);
             this.Controls.Add(this.pictureBox1);
             this.Name = "frmInicio_Dashboard";
             this.Text = "frmInicio_Dashboard";
@@ -58,5 +73,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnManualUso;
     }
 }
--- SistemaInventarioWF/frmInicio_Dashboard.cs
+++ SistemaInventarioWF/frmInicio_Dashboard.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,60 @@ namespace SistemaInventarioWF
         public frmInicio_Dashboard()
         {
             InitializeComponent();
+            btnManualUso.Click += btnManualUso_Click;
+        }
+
+        private void btnManualUso_Click(object sender, EventArgs e)
+        {
+            string rutaManual = ObtenerRutaManualUso();
+
+            if (string.IsNullOrWhiteSpace(rutaManual) || !File.Exists(rutaManual))
+            {
+                MessageBox.Show(
+                    "No se encontro el archivo ManualDeUso-GUSTO.pdf.",
+                    "Manual de uso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = rutaManual,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "No se pudo abrir el manual de uso: " + ex.Message,
+                    "Manual de uso",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private string ObtenerRutaManualUso()
+        {
+            string[] candidatos =
+            {
+                Path.GetFullPath(Path.Combine(Application.StartupPath, "ManualDeUso-GUSTO.pdf")),
+                Path.GetFullPath(Path.Combine(Application.StartupPath, "assets", "documentacion", "ManualDeUso-GUSTO.pdf")),
+                Path.GetFullPath(Path.Combine(Application.StartupPath, "..", "..", "..", "assets", "documentacion", "ManualDeUso-GUSTO.pdf")),
+                Path.GetFullPath(Path.Combine(Application.StartupPath, "..", "..", "assets", "documentacion", "ManualDeUso-GUSTO.pdf"))
+            };
+
+            foreach (string candidato in candidatos)
+            {
+                if (File.Exists(candidato))
+                {
+                    return candidato;
+                }
+            }
+
+            return candidatos[2];
         }
     }
 }
